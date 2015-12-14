bd0516a9f0bbe720a43fd0a7874f1b7a34fdb012
Dylan Waij <d_way@live.nl>
2015-12-14 11:35:18 +0100

    Main

---
+++ Assets/Scripts/backAndForth.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class backAndForth : MonoBehaviour {
+
+	public float delta = 1.5f;  // Amount to move left and right from the start point
+	public float speed = 2.0f;
+	public bool zposition = false;
+	public bool xposition = false;
+	public bool yposition = false;
+	private Vector3 startPos;
+
+	public float timer2;
+	public float maxTime2;
+	
+	void Start () {
+		startPos = transform.position;
+	}
+	
+	void Update () {
+		StartCoroutine (move ());
+	}
+	void mover()
+	{
+		Vector3 pos = startPos;
+		if (zposition == true) {
+			pos.z += delta * Mathf.Sin (Time.time * speed);
+		} if (xposition == true) {
+			pos.x += delta * Mathf.Sin (Time.time * speed);
+		} if (yposition == true) {
+			pos.y += delta * Mathf.Sin (Time.time * speed);
+		}
+		transform.position = pos;
+	}
+    IEnumerator move()
+    {
+    if (timer2 < maxTime2) 
+	{
+		mover();
+		timer2 = timer2 +1;
+	} 
+	if (timer2 >= maxTime2) {
+		yield return new WaitForSeconds(0.01f);
+		timer2 = timer2 = 0;
+	}
+    }
+}
---
+++ Assets/Scripts/turnObject.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class turnObject : MonoBehaviour {
+
+	public float TurnSpeed;
+	public float speed = 2.0f; 
+	private Vector3 startPos;
+	public bool turnRight = false;
+	public bool turnLeft = false;
+
+	public float timer1;
+	public float maxTime1;
+	
+	void Start () {
+		startPos = transform.position;
+	}
+	
+	void Update () {
+		StartCoroutine (Turn ());
+	}
+	void Turning()
+	{
+		if (turnRight == true) {
+			transform.Rotate (Vector3.right * TurnSpeed);
+		} if (turnLeft == true) {
+			transform.Rotate (Vector3.left * TurnSpeed);
+		}
+
+		//transform.Rotate(Vector3.up * TurnSpeed, Space.World);
+	}
+	IEnumerator Turn()
+	{
+		if (timer1 < maxTime1) 
+		{
+			Turning();
+			timer1 = timer1 +1;
+		} 
+		if (timer1 >= maxTime1) {
+			yield return new WaitForSeconds(0.01f);
+			timer1 = timer1 = 0;
+		}
+	}
+}
